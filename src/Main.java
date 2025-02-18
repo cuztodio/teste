1b7628d23710b6797bb4b0fdac6c68130aeb5e83
pedro.custodio <custodiou07@gmail.com>
2025-02-18 09:06:40 -0300

    calculadora.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado = soma(2, 3);
+        System.out.printf("%d", resultado);
+    }
+    public static int soma(int a, int b){
+        return a + b;
+    }
+}
