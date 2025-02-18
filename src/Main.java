94985576b90fa22a4f873e333622893632413f26
pedro.custodio <custodiou07@gmail.com>
2025-02-18 09:37:58 -0300

    metodo sub

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int resultado = soma(2, 3);
-        System.out.printf("%d", resultado);
+        int resultado2 = sub(5, 4);
+        System.out.printf("%d, %d", resultado, resultado2);
     }
     public static int soma(int a, int b){
         return a + b;
+    }
+    public static int sub(int a, int b){
+        return a - b;
     }
 }
